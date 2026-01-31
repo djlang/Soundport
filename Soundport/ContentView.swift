473ec92cbf44500593c874e9fd62fc44a71be6be
dengjinlang <djlang1024@163.com>
2026-01-31 22:13:50 +0800

    Initial Commit

---
+++ Soundport/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Soundport
+//
+//  Created by dengjinlang on 2026/1/31.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Soundport/SoundportApp.swift
@@ -0,0 +1,17 @@
+//
+//  SoundportApp.swift
+//  Soundport
+//
+//  Created by dengjinlang on 2026/1/31.
+//
+
+import SwiftUI
+
+@main
+struct SoundportApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
